49e5995042abf31b2f898ca1daaf7ee99005dde9
plamenbelev <kasa6teka@gmail.com>
2019-05-19 14:28:32 +0300

    start project

--- headlines.py
+++ headlines.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return 'Hello, World!'
+def get_news():
+    return 'No news is good news'
 
 
 if __name__ == '__main__':
